363651af228a1c2b9c85d1c8023d23eca5c3dd9f
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-03-20 08:08:49 +0300

    Added Bootstrap class

--- src/main/java/name/nicholasgribanov/Spring5MvcRestApplication.java
+++ src/main/java/name/nicholasgribanov/Spring5MvcRestApplication.java
@@ -1,4 +1,4 @@
-package name.nicholasgribanov.spring5mvcrest;
+package name.nicholasgribanov;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/name/nicholasgribanov/domain/Category.java
+++ src/main/java/name/nicholasgribanov/domain/Category.java
@@ -1,12 +1,14 @@
 package name.nicholasgribanov.domain;
 
 import lombok.Data;
-import org.springframework.data.annotation.Id;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
+@Entity
 public class Category {
 
     @Id
