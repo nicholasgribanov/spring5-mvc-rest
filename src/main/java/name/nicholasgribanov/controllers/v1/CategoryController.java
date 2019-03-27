ae610b93c39d0b522334bf1b90b5cedf179f8ca5
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-03-27 07:59:38 +0300

    Added Category Controller

---
+++ src/main/java/name/nicholasgribanov/controllers/v1/CategoryController.java
@@ -0,0 +1,34 @@
+package name.nicholasgribanov.controllers.v1;
+
+import name.nicholasgribanov.api.v1.model.CategoryDTO;
+import name.nicholasgribanov.api.v1.model.CategoryListDTO;
+import name.nicholasgribanov.services.CategoryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/v1/categories")
+public class CategoryController {
+
+    private final CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping
+    public ResponseEntity<CategoryListDTO> getAllCategories() {
+        return new ResponseEntity<CategoryListDTO>(
+                new CategoryListDTO(categoryService.getAllCategories()), HttpStatus.OK);
+    }
+
+    @GetMapping("{name}")
+    public ResponseEntity<CategoryDTO> getCategoryByName(@PathVariable String name) {
+        return new ResponseEntity<CategoryDTO>(
+                categoryService.getCategoryByName(name), HttpStatus.OK);
+    }
+}
--- src/main/java/name/nicholasgribanov/services/CategoryServiceImpl.java
+++ src/main/java/name/nicholasgribanov/services/CategoryServiceImpl.java
@@ -3,10 +3,12 @@ package name.nicholasgribanov.services;
 import name.nicholasgribanov.api.v1.mapper.CategoryMapper;
 import name.nicholasgribanov.api.v1.model.CategoryDTO;
 import name.nicholasgribanov.repositories.CategoryRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
     private final CategoryMapper categoryMapper;
