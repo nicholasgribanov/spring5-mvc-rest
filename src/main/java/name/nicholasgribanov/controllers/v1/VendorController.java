ba223bb7f1c22f9475642dd808ef5a05e0749430
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-04-08 08:18:13 +0300

    Added POST, PATCH, PUT and DELETE methods for Vendor Controller

--- src/main/java/name/nicholasgribanov/controllers/v1/VendorController.java
+++ src/main/java/name/nicholasgribanov/controllers/v1/VendorController.java
@@ -1,12 +1,10 @@
 package name.nicholasgribanov.controllers.v1;
 
+import name.nicholasgribanov.api.v1.model.VendorDTO;
 import name.nicholasgribanov.api.v1.model.VendorListDTO;
 import name.nicholasgribanov.services.VendorService;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(VendorController.BASE_URL)
@@ -24,4 +22,29 @@ public class VendorController {
     public VendorListDTO getAllVendors() {
         return new VendorListDTO(vendorService.getAllVendors());
     }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public VendorDTO addVendor(@RequestBody VendorDTO vendorDTO) {
+        return vendorService.createNewVendor(vendorDTO);
+    }
+
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public VendorDTO patchVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO) {
+        return vendorService.patchVendor(id, vendorDTO);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public VendorDTO updateVendor(@PathVariable Long id, @RequestBody VendorDTO vendorDTO) {
+        return vendorService.saveVendorByDTO(id, vendorDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public void deleteVendor(@PathVariable Long id) {
+        vendorService.deleteVendorById(id);
+    }
+
 }
