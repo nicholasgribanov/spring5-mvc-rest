1d31b171aecb17305c5a370a1a7098e1559cacb7
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2019-04-09 07:59:23 +0300

    Added metadata to Swagger

--- src/main/java/name/nicholasgribanov/config/SwaggerConfig.java
+++ src/main/java/name/nicholasgribanov/config/SwaggerConfig.java
@@ -2,15 +2,21 @@ package name.nicholasgribanov.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import java.util.ArrayList;
+
 @EnableSwagger2
 @Configuration
-public class SwaggerConfig {
+public class SwaggerConfig /*extends WebMvcConfigurationSupport*/ {
 
     @Bean
     public Docket api() {
@@ -18,6 +24,34 @@ public class SwaggerConfig {
                 .select().apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .pathMapping("/");
+                .pathMapping("/")
+                .apiInfo(metaData());
     }
+
+    private ApiInfo metaData() {
+        Contact contact = new Contact("Nikolay Gribanov",
+                "http://nicholasgribanov.name",
+                "nicholasg.ix35@gmail.com");
+
+        return new ApiInfo(
+                "RESTfull API",
+                "A little example RESTfull service",
+                "1.0",
+                "Terms of service: cool",
+                contact,
+                "Apache License Version 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>()
+        );
+    }
+
+/*
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }*/
 }
